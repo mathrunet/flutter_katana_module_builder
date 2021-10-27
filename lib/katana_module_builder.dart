9c45f2bd7b89829f254e5fb52b3d8365bd389b55
mathru <user@mathru.net>
2021-10-27 13:10:09 +0900

    最適化

--- lib/katana_module_builder.dart
+++ lib/katana_module_builder.dart
@@ -11,12 +11,12 @@ library katana_module_builder;
 import 'dart:async';
 
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
 import 'package:build/build.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:dart_style/dart_style.dart';
-import 'package:source_gen/source_gen.dart';
-import 'package:analyzer/dart/element/type.dart';
 import 'package:katana_module_builder_annotation/katana_module_builder_annotation.dart';
+import 'package:source_gen/source_gen.dart';
 
 part 'src/builder.dart';
 part 'src/extensions.dart';
